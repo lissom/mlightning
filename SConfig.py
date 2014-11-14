356285d0ad31ad684dfd48b0acd3225732ac9545
charlie.page <charlie.page@gmail.com>
2014-11-14 15:37:57 -0500

    Sconfig.py added (lost in rebrand)

---
+++ SConfig.py
@@ -0,0 +1,22 @@
+# THIS FILE WAS AUTOMATICALLY GENERATED. MANUAL CHANGES IN THIS FILE WILL BE LOST AFTER THE NEXT C/C++ PROJECT CHANGES!!!
+SCONS_OPTIONS = {'num_jobs':'8'}
+DECIDER = 'MD5'
+COMP_INCLUDES_INTO_CCFLAGS = False
+BUILD_ARTIFACT_NAME = 'mload'
+PROJECT_TYPE = 'exe'
+BUILD_CONFIGURATION = 'Debug'
+TOOLCHAIN_NAME = 'linux gcc'
+C_FLAGS = '-O0 -g3 -Wall -c -fmessage-length=0'
+CXX_FLAGS = '-std=c++0x -O0 -g3 -p -Wall -c -fmessage-length=0'
+COMPILER_NAME = 'g++'
+PROJECT_NAME = 'mload'
+INCLUDES = []
+LIBRARIES = ['tcmalloc', 'mongoclient', 'boost_program_options', 'boost_system', 'boost_regex', 'boost_thread', 'boost_chrono', 'boost_filesystem']
+LIBRARY_PATHS = []
+SOURCE_PATHS = {'src':[]}
+PRE_BUILD_COMMAND = ''
+PRE_BUILD_DESC = ''
+POST_BUILD_COMMAND = ''
+POST_BUILD_DESC = ''
+COMPILER_DEFINES = {}
+LINKER_FLAGS = ''
